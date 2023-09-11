39db60ecfc042bd943ace648dde8557d33d4a433
tayhoang64 <tayhoang64@gmail.com>
2023-09-11 11:22:41 +0700

    change password

--- assets/profile/profile.js
+++ assets/profile/profile.js
@@ -6,7 +6,15 @@ let regexPhoneNumber = /(84|0[3|5|7|8|9])+([0-9]{8})\b/g;
 const dateString = document.getElementById('created-at').innerText;
 const dateArray = dateString.split(" "); 
 const datePortion = dateArray[0];
-const parts = datePortion.split("-"); 
+const parts = datePortion.split("-");
+let changePasswordbtn = document.getElementById('change-password-save-btn')
+const isNonWhiteSpace = /^\S*$/;
+const isContainsUppercase = /^(?=.*[A-Z]).*$/;
+const isContainsLowercase = /^(?=.*[a-z]).*$/;
+const isContainsNumber = /^(?=.*[0-9]).*$/;
+const isContainsSymbol = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
+const isValidLength = /^.{6,16}$/;
+let regularExpression = /^(\S)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])[a-zA-Z0-9~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]{6,16}$/;
 
 
 const formattedDate = `${parts[2]}/${parts[1]}/${parts[0]}`;
@@ -113,3 +121,76 @@ updateInfoBtn.onclick = (e) => {
   });
   return;
 }
+
+changePasswordbtn.onclick = (e =>{
+  e.preventDefault();
+  let check = true;
+  changePasswordHtml = document.getElementById('changepass-form');
+  let formData = new FormData(changePasswordHtml);
+  let oldPassword = formData.get('oldpassword');
+  let newPassword = formData.get('newpassword');
+  let confirmNewPassword = formData.get('cnewpassword');
+
+
+  if(!isNonWhiteSpace.test(newPassword)){
+    document.getElementById('new-password-error').innerHTML ='Password must not contain Whitespaces.'
+    check = false;
+  }
+  if (!isContainsUppercase.test(newPassword)){
+    document.getElementById('new-password-error').innerHTML ='Password must have at least one Uppercase Character.'
+    check = false;
+  }
+  if (!isContainsLowercase.test(newPassword)){
+    document.getElementById('new-password-error').innerHTML ='Password must have at least one Lowercase Character.'
+    check = false;
+  }
+  if(!isContainsNumber.test(newPassword)){
+    document.getElementById('new-password-error').innerHTML ='Password must have at least one Number'
+    check = false;
+  }
+  if(!isContainsSymbol.test(newPassword)){
+    document.getElementById('new-password-error').innerHTML ='Password must have at least one Special Symbol.'
+    check = false;
+  }
+  if(!isValidLength.test(newPassword)){
+    document.getElementById('new-password-error').innerHTML ='Password must be 6-16 Characters Long.'
+    check = false;
+  }
+  if(regularExpression.test(newPassword)){
+    document.getElementById('new-password-error').innerHTML ='';
+  }
+
+
+  if(confirmNewPassword != newPassword){
+    document.getElementById('confirm-new-password-error').innerHTML = 'Passwords do not match.'
+    check = false;
+  }else{
+      document.getElementById('confirm-new-password-error').innerHTML = ''
+  }
+
+  if(check==false){
+    return false;
+  }
+
+  $.ajax({
+    type : "POST",
+    url : "/api/users/reset-password",
+    data : {
+      old_password : oldPassword,
+      new_password : newPassword,
+      confirm_new_password : confirmNewPassword
+    },
+    dataType: "JSON",
+    success: function (response) {
+      Swal.fire(
+          'Nice!',
+          'Password changed successfully!',
+          'success'
+      )
+    },
+    error: function (changePassword){
+      document.getElementById('old-password-error').innerHTML = `Please enter the correct password`
+    }
+  })
+  return;
+})
